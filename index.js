9cf2fccd275d1457bda94993bf443985022554f2
Wali Rahman <wali.rahman89@yahoo.com>
2017-11-26 17:18:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,5 @@ const createFareMultiplier = function (integer) {
 const fareDoubler = createFareMultiplier (2);
 
 const fareTripler = createFareMultiplier (3);
+
+const selectingDifferentDrivers = function (drivers, selectingDrivers)
